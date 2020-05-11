07c78980ca3f7881ecde6fe9f5f9ab74345e7f53
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-11 19:00:28 +0200

    feat: mailer v3

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+.)+[^@\s]+\z/, message: "Adresse email s'il te plait" }
+  validates :encrypted_password, presence: true, length: {in: 6..20}
+  validates :description, length: {maximum: 250}, presence: true
+  validates :first_name, length: {in: 3..14}, presence: true
+  validates :last_name, length: {in: 3..14}, presence: true
+
+  has_many :events, foreign_key: 'admin_id', class_name: 'Event' #pas sur de cette ligne 
+  has_many :attendances
+  has_many :events, through: :attendances
+
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+end
