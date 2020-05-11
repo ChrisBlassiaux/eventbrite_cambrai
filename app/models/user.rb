0cf4fa9e8e5f594c8ffd0af4cb2f6821c91e3392
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-11 19:48:43 +0200

    feat: mailer v4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+.)+[^@\s]+\z/, message: "Adresse email s'il te plait" }
-  # validates :encrypted_password, presence: true, length: {in: 6..20}
-  # validates :description, length: {maximum: 250}, presence: true
-  # validates :first_name, length: {in: 3..14}, presence: true
-  # validates :last_name, length: {in: 3..14}, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+.)+[^@\s]+\z/, message: "Adresse email s'il te plait" }
+  validates :encrypted_password, presence: true, length: {in: 6..20}
+  validates :description, length: {maximum: 250}, presence: true
+  validates :first_name, length: {in: 3..14}, presence: true
+  validates :last_name, length: {in: 3..14}, presence: true
 
   has_many :events, foreign_key: 'admin_id', class_name: 'Event' #pas sur de cette ligne 
   has_many :attendances
