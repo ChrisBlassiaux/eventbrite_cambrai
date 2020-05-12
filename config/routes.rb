2111a3b3b6d2aa92cc0865c190617626fe834165
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-12 10:46:09 +0200

    feat: 2. 2.1 2.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  root 'events#index'
+  #resources :events, only: [:new]
+  #resources :users, only: [:show]
+  devise_for :users
+ end
