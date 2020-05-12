57e2e465b8d1a81fae5868d6f30977a1db504c1f
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-13 00:09:36 +0200

    front v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  # after_create :welcome_send   Le décommenter quand le mailer sera reparé
+  #after_create :welcome_send
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
