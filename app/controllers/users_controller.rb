61141401b36783c4c239a9bd61752e0bf4e750bf
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-12 22:46:21 +0200

    feat: 2.3.2 profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user, only: [:show]
+  before_action :his_profil, only: [:show]
+
   def index
     @users = User.all
   end
 
   def show
-    #@user = User.find(params[:id])
+  end
+
+  private 
+
+  def authenticate_user
+    unless current_user
+      flash[:echec] = "Connecte toi pour voir ton profil !"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def his_profil
+    if current_user.id == params[:id]
+      flash[:echec] = "Connecte toi pour voir ton profil !"
+      redirect_to new_user_session_path
+    end
   end
 end
