b74309afd54b6e6d0950b7252dc3557ba90b242f
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-12 21:12:11 +0200

    fix: link login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'events#index'
-  resources :events, only: [:new, :create, :show]
-  resources :users, only: [:show]
   devise_for :users
+  resources :events, only: [:new, :create, :show]
+  resources :users
  end
