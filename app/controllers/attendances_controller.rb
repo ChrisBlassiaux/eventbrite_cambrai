693e38088a37f939c2fb63d34adda0553b996c22
ChrisBlassiaux <chrisblassiaux@gmail.com>
2020-05-15 12:29:24 +0200

    doc: asset pipeline

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,17 +1,37 @@
 class AttendancesController < ApplicationController
+  def index
+  end
+
   def new
     @attendances = Attendance.all
   end
 
   def create 
-    @attendance = Attendance.new(strip_customer_id: '5489', user_id: current_user.id, event_id: params[:format])
+      # Amount in cents
+    @event = Event.find(params[:format]).price
+    @amount = @event * 100
+  
+    customer = Stripe::Customer.create({
+      email: params[:stripeEmail],
+      source: params[:stripeToken],
+    })
+
+    @attendance = Attendance.new(strip_customer_id: customer.id, user_id: current_user.id, event_id: params[:format])
+  
+    charge = Stripe::Charge.create({
+      customer: customer.id,
+      amount: @amount,
+      description: "Paiement de l'évènement",
+      currency: 'eur',
+    })
 
     if @attendance.save 
       flash[:success] = "Vous êtes inscrit pour cet évènement."
       redirect_to event_path(params[:format])
-    else
-      flash[:echec] = "Vous n'êtes pas inscrit pour cet évènement, veuillez ré-essayer."
-      redirect_to event_path(params[:format])
     end
+  
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to event_path(params[:format])
   end
 end
